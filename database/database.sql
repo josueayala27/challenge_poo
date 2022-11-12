509c9c8c5e024494bfc30d41b43bfe5d098e65c4
Josué Ayala <104921046+jayalaa@users.noreply.github.com>
2022-11-12 10:10:45 -0600

    Actualizacion `database.sql`

--- database/database.sql
+++ database/database.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `user_roles` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `users` (
@@ -12,64 +12,64 @@ CREATE TABLE `users` (
   `full_name` varchar(255),
   `password` varchar(255),
   `user_role` int,
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `password_resets` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `token` varchar(255),
-  `user_id` id,
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `user_id` int,
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `locations` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `location` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `location_sections` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `section` varchar(255),
   `location_id` int,
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `copy_categories` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `category` varchar(255),
   `location_section_id` int,
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `copy_authors` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
-  `author` name,
-  `birth_date` timestamp,
-  `death_date` timestamp,
+  `author` varchar(255),
+  `birth_date` timestamp DEFAULT (now()),
+  `death_date` timestamp DEFAULT (now()),
   `description` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `copy_type` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `type` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `editorials` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `editorial` varchar(255),
   `phone` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `copies` (
@@ -77,28 +77,28 @@ CREATE TABLE `copies` (
   `isbn` varchar(255),
   `name` varchar(255),
   `quantity` int,
-  `publish_at` timestamp,
+  `publish_at` timestamp DEFAULT (now()),
   `total_pages` int,
   `editorial_id` int,
   `copy_category` int,
   `copy_author` int,
   `copy_type` int,
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `interest_rates` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `percent` double,
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `loan_plan_types` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `recurrence` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `loan_plans` (
@@ -106,8 +106,8 @@ CREATE TABLE `loan_plans` (
   `loan_plan_type` int,
   `count` int,
   `type` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 CREATE TABLE `copy_loans` (
@@ -118,9 +118,9 @@ CREATE TABLE `copy_loans` (
   `copy_id` int,
   `loan_plan` int,
   `delay_percent` int,
-  `return_date` timestamp,
-  `created_at` timestamp,
-  `updated_at` timestamp
+  `return_date` timestamp DEFAULT (now()),
+  `created_at` timestamp DEFAULT (now()),
+  `updated_at` timestamp DEFAULT (now())
 );
 
 ALTER TABLE `users` ADD FOREIGN KEY (`user_role`) REFERENCES `user_roles` (`id`);
